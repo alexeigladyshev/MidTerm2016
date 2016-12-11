72a0d29b0cbcb2c23b3837ea616360028da1c53a
mrahman <rahmanww@gmail.com>
2016-12-11 12:52:22 -0500

    exam setup

---
+++ src/core/java/OOP.java
@@ -0,0 +1,11 @@
+package core.java;
+
+/**
+ * Created by PIIT_NYA on 12/11/2016.
+ */
+public class OOP {
+
+    public static void main(String[] args) {
+        System.out.println("This is OOP feature test");
+    }
+}
---
+++ src/data/structure/Sorting.java
@@ -0,0 +1,10 @@
+package data.structure;
+
+/**
+ * Created by PIIT_NYA on 12/11/2016.
+ */
+public class Sorting {
+    public static void main(String[] args) {
+        System.out.println("This is sorting algo");
+    }
+}
